075b4286a68c98f66a0ef9bb3305f6a80c515f34
RDabulle <47108449+RDabulle@users.noreply.github.com>
2022-08-05 14:51:25 +0900

    mission11

--- mission11/index.js
+++ mission11/index.js
@@ -1,15 +1,15 @@
-// function a() {
-//     console.log("a")
-//     a()
-// }
-// a()
+function sleep(time) {
+    return new Promise((resolve) => {setTimeout(() => {resolve()}, time);})
+}
 
+async function run() {
+    console.log("1")
+    await sleep(1000)
+    console.log("2")
+    await sleep(1000)
+    console.log("3")
+    await sleep(1000)
+    console.log("4")
+}
 
-// const b = () => {
-//     console.log("b")
-//     b()
-// }
-// b()
-
-
-console.log( typeof null )
+run()
