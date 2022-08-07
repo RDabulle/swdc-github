133062ff90f5decb04bae31167bdcdfaa58bd126
RDabulle <47108449+RDabulle@users.noreply.github.com>
2022-08-07 19:53:03 +0900

    mission14

--- mission14/app.js
+++ mission14/app.js
@@ -3,7 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('get')
+})
+
+app.post('/', (req, res) => {
+  res.send('post')
+})
+
+app.delete('/', (req, res) => {
+  res.send('delete')
+})
+
+app.patch('/', (req, res) => {
+  res.send('patch')
 })
 
 app.listen(port, () => {})
